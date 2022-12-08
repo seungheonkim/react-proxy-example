21f535edb3ba0f468b2b8575ef5024b76a662820
seungheonkim <seungheon818@naver.com>
2022-12-08 23:55:57 +0900

    api-done

--- my-app/src/setUpProxy.js
+++ my-app/src/setUpProxy.js
@@ -1,18 +1,28 @@
 const {createProxyMiddleware} = require('http-proxy-middleware');
 
 module.exports = function (app) {
+    // app.use(
+    //     '/api',
+    //     createProxyMiddleware({
+    //         target: 'http://localhost:3080',
+    //         changeOrigin: true,
+    //     })
+    // )
+    // app.use(
+    //     '/api2',
+    //     createProxyMiddleware({
+    //         target: 'http://localhost:3070',
+    //         changeOrigin: true,
+    //     })
+    // )
     app.use(
-        '/api',
+        ['/api', '/api2'],
         createProxyMiddleware({
             target: 'http://localhost:3080',
             changeOrigin: true,
-        })
-    )
-    app.use(
-        '/api2',
-        createProxyMiddleware({
-            target: 'http://localhost:3070',
-            changeOrigin: true,
+            router: {
+                '/api2': 'http://localhost:3070'
+            }
         })
     )
 }
